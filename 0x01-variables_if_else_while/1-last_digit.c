3586ca46c92dbb3d4430c9dfbb8494f29a1282eb
your Name <you@example.com>
2022-12-11 11:14:12 -0800

    3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,10 @@
 #include<stdio.h> 
 /* more headers goes there */                               
 
-/* betty style doc for function main goes there */          
+/**
+ *main -Entry point
+ *return: Always 0 (success)
+ */          
 int main(void)
 {
 	int n, lastNum;
