233a5789c3e5b695cdca9bcc377576f903acb6ee
your Name <you@example.com>
2023-01-06 10:40:13 -0800

    all

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,12 +22,15 @@ int check_num(char *str)
 		count++;
 		return (1);
 	}
-
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
-	int sum = 0;
+	int count, str_to_int, sum = 0;
 
 	count = 1;
 	while (count < argc)
